d3438afa67913c4a0eadbee29a862189e4fa6abe
Rasmus Lindroth <rasmus@lindroth.xyz>
2018-11-15 15:59:57 +0100

    Follow redirects

--- omdb.class.php
+++ omdb.class.php
@@ -126,6 +126,7 @@ class OMDb {
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_TIMEOUT, $this->timeout);
+	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         $content = curl_exec($ch);
         $info = curl_getinfo($ch);
